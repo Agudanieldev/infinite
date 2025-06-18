415e7bca5ab9701f2347b0ebc2783cec031fcc75
Agudaniel <adofintl@gmail.com>
2025-06-18 14:51:37 +0100

    adjusted-gtrans-js

--- js/googletranscookie.js
+++ js/googletranscookie.js
@@ -1,4 +1,34 @@
 
+  // function googleTranslateElementInit() {
+  //   new google.translate.TranslateElement({
+  //     pageLanguage: 'en',
+  //     includedLanguages: 'en,es,fr,de',
+  //     layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+  //   }, 'google_translate_element');
+  // }
+
+  // function setLanguage(lang) {
+  //   document.cookie = `googtrans=/en/${lang};path=/`;
+  //   location.reload();
+  // }
+
+  // function customizeGoogleTranslateLabel() {
+  //   const interval = setInterval(() => {
+  //     const selectEl = document.querySelector('#google_translate_element select');
+  //     if (selectEl) {
+  //       const firstOption = selectEl.querySelector('option');
+  //       if (firstOption && firstOption.innerHTML.includes('Select Language')) {
+  //         firstOption.innerHTML = 'English';
+  //         clearInterval(interval);
+  //       }
+  //     }
+  //   }, 100);
+  // }
+
+  // window.addEventListener('load', customizeGoogleTranslateLabel);
+
+
+    // Initialize Google Translate
   function googleTranslateElementInit() {
     new google.translate.TranslateElement({
       pageLanguage: 'en',
@@ -7,23 +37,13 @@
     }, 'google_translate_element');
   }
 
+  // Set language cookie
   function setLanguage(lang) {
     document.cookie = `googtrans=/en/${lang};path=/`;
-    location.reload();
   }
 
-  function customizeGoogleTranslateLabel() {
-    const interval = setInterval(() => {
-      const selectEl = document.querySelector('#google_translate_element select');
-      if (selectEl) {
-        const firstOption = selectEl.querySelector('option');
-        if (firstOption && firstOption.innerHTML.includes('Select Language')) {
-          firstOption.innerHTML = 'English';
-          clearInterval(interval);
-        }
-      }
-    }, 100);
-  }
-
-  window.addEventListener('load', customizeGoogleTranslateLabel);
+  // Auto-translate to French on page load (change 'fr' to your target language code)
+  window.addEventListener('load', function () {
+    setLanguage('fr'); // Change to 'es', 'de', etc. as needed
+  });
 
